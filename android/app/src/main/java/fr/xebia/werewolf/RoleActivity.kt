9e56a39c5b3fe3c6736899b5a82f747251409d7d
Qian Jin <jinqian.hz@gmail.com>
2017-06-26 17:08:19 +0200

    [android] add the round 1 start trigger

--- android/app/src/main/java/fr/xebia/werewolf/RoleActivity.kt
+++ android/app/src/main/java/fr/xebia/werewolf/RoleActivity.kt
@@ -1,5 +1,6 @@
 package fr.xebia.werewolf
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -8,26 +9,29 @@ import android.view.MotionEvent.ACTION_DOWN
 import android.view.MotionEvent.ACTION_UP
 import android.view.View.GONE
 import android.view.View.VISIBLE
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 import fr.xebia.werewolf.R.string.waiting_for_role_prompt
 import fr.xebia.werewolf.model.PlayerState
 import fr.xebia.werewolf.model.Role
+import fr.xebia.werewolf.model.Round
+import fr.xebia.werewolf.round.NightActivity
 import kotlinx.android.synthetic.main.activity_role.*
 
 class RoleActivity : AppCompatActivity() {
 
     private var givenRole: Role = Role.EMPTY
+
+    private lateinit var gameId: String
+    private lateinit var playerName: String
     private lateinit var currentPlayerRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_role)
 
-        val gameId = prefsUtil.currentGameId
-        val playerName = prefsUtil.currentPlayerName
+        gameId = prefsUtil.currentGameId
+        playerName = prefsUtil.currentPlayerName
+
         currentPlayerRef = firebaseDbRef.child("games/$gameId/players/$playerName")
         roleWaitingPrompt.text = String.format(getString(waiting_for_role_prompt), playerName)
 
@@ -86,6 +90,36 @@ class RoleActivity : AppCompatActivity() {
                 } catch(e: IllegalArgumentException) {
                     Log.e("WEREWOLF", "Role type error")
                 }
+            }
+        })
+
+        setupRoundListener()
+    }
+
+    private fun setupRoundListener() {
+        val currentRoundRef = firebaseDbRef.child("games/$gameId/rounds")
+        currentRoundRef.addChildEventListener(object : ChildEventListener {
+            override fun onCancelled(p0: DatabaseError?) {
+                // void
+            }
+
+            override fun onChildMoved(p0: DataSnapshot?, p1: String?) {
+                // void
+            }
+
+            override fun onChildChanged(p0: DataSnapshot?, p1: String?) {
+                // void
+            }
+
+            override fun onChildAdded(p0: DataSnapshot?, p1: String?) {
+                val currentRound = p0!!.getValue(Round::class.java)
+                if (currentRound!!.number == 1) {
+                    startActivity(Intent(this@RoleActivity, NightActivity::class.java))
+                }
+            }
+
+            override fun onChildRemoved(p0: DataSnapshot?) {
+                // void
             }
         })
     }
---
+++ android/app/src/main/java/fr/xebia/werewolf/model/Phase.kt
@@ -0,0 +1,4 @@
+package fr.xebia.werewolf.model
+
+data class Phase(val state: String = "",
+                 val subPhase: SubPhase = SubPhase())
