c2e9d7615f0b48eca4e3cb698fa6b2bbac66f717
jsmadja <julien.smadja@gmail.com>
2017-07-02 17:39:05 +0200

    Testing Votes

--- backend/app/core/votes.js
+++ backend/app/core/votes.js
@@ -1,47 +1,36 @@
 const _ = require('lodash');
 
+const votesEqualTo = maxVotes => player => player.votes === maxVotes;
+
+const toTarget = target =>
+  ({
+    player: target[0].voted,
+    votes: target.length,
+  });
+
 class Votes {
-  constructor(data) {
+  constructor(data = {}) {
     Object.assign(this, data);
   }
 
-  // TO_BE_REVIEWED @jsmadja
   countVotes() {
-    var count = 0;
-    for (var property in this) {
-      if (this.hasOwnProperty(property)) {
-        count++;
-      }
-    }
-    return count;
+    return _.keys(this).length;
   }
 
-  // TO_BE_REVIEWED @jsmadja
   getMajority() {
-    const result = {}
-    // Additionner les voix.
-    for (var property in this) {
-      var count = result[this[property].voted] ? result[this[property].voted] : 0;
-      result[this[property].voted] = count + 1
+    const killables =
+      _(this).valuesIn()
+      .groupBy('voted')
+      .map(toTarget)
+      .orderBy('votes', 'desc')
+      .value();
+    if (killables.length === 0) {
+      return [];
     }
-
-    // Déterminer le maximum
-    var chosenOnes = [];
-    var chosenOneVoteAgainst = 0;
-    for (var property in result) {
-      if (result[property] > chosenOneVoteAgainst) {
-        chosenOnes = [property];
-        chosenOneVoteAgainst = result[property]
-        // Déterminer une éventuelle égalité
-      } else if (result[property] == chosenOneVoteAgainst) {
-        chosenOnes.push(property)
-      }
-    }
-
-    console.log("= Here are the vote results", chosenOnes)
-    return chosenOnes;
+    const playersToKill = _(killables).filter(votesEqualTo(killables[0].votes)).map('player').value();
+    console.log('= Here are the vote results', playersToKill);
+    return playersToKill;
   }
-
 }
 
 module.exports = Votes;
