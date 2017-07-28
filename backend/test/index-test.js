3a08477445e9a08f7d341a3cd0d9364cae09fe78
jsmadja <julien.smadja@gmail.com>
2017-07-28 15:31:53 +0200

    Add test for GET /_ah/health

--- backend/test/index-test.js
+++ backend/test/index-test.js
@@ -9,4 +9,15 @@ describe('integration testing', () => {
     .get('/')
     .expect('Content-Type', 'text/html; charset=utf-8')
     .expect(200, 'Basic page for the Werewolves game'));
+
+  it('should respond to GET /_ah/health', () =>
+    request(app)
+    .get('/_ah/health')
+    .expect(200));
+
+  it.skip('should respond to POST /home', () =>
+    request(app)
+    .post('/home')
+    .expect(200));
+
 });
